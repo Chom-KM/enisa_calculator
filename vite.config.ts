947b65d6e453cc988d278286bb48845d978fc0bc
ohm taweesap <ohmtvsap@gmail.com>
2025-10-01 00:24:58 +0700

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/enisa_calculator_update/', // <- EXACT repo name with trailing slashes
+  base: '/enisa_calculator/', // <- EXACT repo name with trailing slashes
 })
